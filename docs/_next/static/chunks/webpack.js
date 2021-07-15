bb62974ae6de30fe42ee4183215550ea7e01581d
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-07-16 09:51:12 +1200

    cross over to mdx

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fed3e84f26d6dbd71ea5";
+/******/ 	var hotCurrentHash = "2830c2e3f9ffd88f3a6a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
