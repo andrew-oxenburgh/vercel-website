01e6f64897ce186b3b50d83848f173a7e605dd5e
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-07-16 12:09:47 +1200

    fixed some prop-types. removed unused script

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6090c8ee03058bc360b7";
+/******/ 	var hotCurrentHash = "76cbd456bd67f292ad53";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
