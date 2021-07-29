f292342da82b21b7db907a74a51c7011ce5c670b
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-07-29 12:20:44 +1200

    export

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "52b8e222e3db27edfb48";
+/******/ 	var hotCurrentHash = "3fd670aa88aa09e4ea72";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
