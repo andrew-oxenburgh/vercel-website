1d2d1f6bd60a6508a85e449a1fe591d3c124f460
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-09-16 08:50:18 +1200

    Merge branch 'master' of https://github.com/andrew-oxenburgh/vercel-website
    # Conflicts:
    #	src/components/BlogList.tsx
    #	src/components/BlogPost.tsx
    #	src/components/NavBar.tsx
    #	src/pages/_document.js

--- src/components/BlogSummary.stories.jsx
+++ src/components/BlogSummary.stories.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 
-import {CornerStamp} from './CornerStamp';
+import {BlogSummary} from './BlogSummary';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-	title: 'Blog/CornerStamp',
-	component: CornerStamp,
+	title: 'Blog/BlogSummary',
+	component: BlogSummary,
+	argTypes: {
+		backgroundColor: {control: 'color'},
+	},
 };
 
-const Template = args => <CornerStamp {...args} />;
+const Template = args => <BlogSummary {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-	host: 'vercel',
+	post: {
+		link: '/some-link',
+		module: {
+			meta: {
+				date: 'July 27, 2021',
+				title: 'blog title goes here',
+				description: 'here is a desc',
+				readTime: '5',
+			},
+		},
+	},
 };
